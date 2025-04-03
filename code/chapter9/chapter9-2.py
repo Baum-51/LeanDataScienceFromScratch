d81c1f87a7f6d1cb85be2b44011147933d666a27
Baum-51 <olympos.12z@gmail.com>
2025-04-03 20:47:32 +0900

    20250403 9.5はスキップ

--- code/chapter9/chapter9-2.py
+++ code/chapter9/chapter9-2.py
@@ -1,6 +1,10 @@
 import json
 import requests
 
+from collections import Counter
+from dateutil.parser import parse
+
+
 serialized = """{"title":  "Data Science Book",
                  "author": "Joel Grus",
                  "publicationYear": 2019,
@@ -16,3 +20,12 @@ endpoint = f"https://api.github.com/users/{github_user}/repos"
 
 repos = json.loads(requests.get(endpoint).text)
 # print(repos)
+
+dates = [parse(repo["created_at"]) for repo in repos]
+month_counts = Counter(date.month for date in dates)
+weekday_counts = Counter(date.weekday() for date in dates)
+
+last_5_repositories = sorted(repos,
+                             key=lambda r: r["pushed_at"],
+                             reverse=True)[:5]
+last_5_languages = [repo["language"] for repo in last_5_repositories]
