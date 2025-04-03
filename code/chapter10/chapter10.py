870324717eea993d7ac66289e460d96aa71e6e3a
Baum-51 <olympos.12z@gmail.com>
2025-04-03 21:43:03 +0900

    20250403 10.1まで完了

---
+++ code/chapter10/chapter10.py
@@ -0,0 +1,98 @@
+from collections import Counter
+import math
+
+import matplotlib.pyplot as plt
+
+def bucketize(point: float, bucket_size: float) -> float:
+    """pointの値を切り捨ててバケツの下限の値を揃える"""
+    return bucket_size * math.floor(point / bucket_size)
+
+def make_histogram(points: list[float], bucket_size: float) -> dict[float, int]:
+    """pointをバケツに入れ、何個入ったか数える"""
+    return Counter(bucketize(point, bucket_size) for point in points)
+
+def plot_histogram(points: list[float], bucket_size: float, title: str=''):
+    histogram = make_histogram(points, bucket_size)
+    plt.bar(histogram.keys(), histogram.values(), width=bucket_size)
+    plt.title(title)
+    
+import random
+import os
+import sys
+sys.path.append(os.getcwd())
+from scratch.probability import inverse_normal_cdf
+
+random.seed(0)
+
+# -100から100までの一様分布
+uniform = [200 * random.random() - 100 for _ in range(10000)]
+
+# 平均0、標準偏差57の正規分布
+normal = [57 * inverse_normal_cdf(random.random()) for _ in range(10000)]
+
+plt.figure()
+plot_histogram(uniform, 10, "Uniform Histogram")
+plt.savefig('../picture/chap10-1.png')
+plt.close()
+
+plt.figure()
+plot_histogram(normal, 10, "Uniform Histogram")
+plt.savefig('../picture/chap10-2.png')
+plt.close()
+
+def random_normal() -> float:
+    """標準正規分布に従う無作為の数を返す"""
+    return inverse_normal_cdf(random.random())
+
+xs = [random_normal() for _ in range(1000)]
+ys1 = [ x + random_normal() / 2 for x in xs]
+ys2 = [-x + random_normal() / 2 for x in xs]
+
+plt.figure()
+plt.scatter(xs, ys1, marker='.', color='black', label='ys1')
+plt.scatter(xs, ys2, marker='.', color='gray',  label='ys2')
+plt.xlabel('xs')
+plt.ylabel('ys')
+plt.legend(loc=9)
+plt.title('Very Different Joint Distributions')
+plt.savefig('../picture/chap10-3.png')
+plt.close()
+
+from scratch.statistics import correlation
+
+print(correlation(xs, ys1))
+print(correlation(xs, ys2))
+
+from scratch.linear_algebra import Matrix, Vector, make_matrix
+
+def correlation_matrix(data: list[Vector]) -> Matrix:
+    """
+    (i, j)番目のエントリがdata[i]とdata[j]の相関となるような
+    len(data) x len(data)行列を返す
+    """
+    def correlation_ij(i: int, j: int) -> float:
+        return correlation(data[i], data[j])
+    
+    return make_matrix(len(data), len(data), correlation_ij)
+
+# corr_dataは、4つの100次元ベクトルのリスト
+from scratch.working_with_data import corr_data
+num_vectors = len(corr_data)
+fig, ax = plt.subplots(num_vectors, num_vectors)
+
+for i in range(num_vectors):
+    for j in range(num_vectors):
+        # x軸のcolumn_j、y軸のcolumn_iの位置に散布図を描画する
+        if i != j: ax[i][j].scatter(corr_data[j], corr_data[i])
+        # i == jであれば、列名を表示する
+        else: ax[i][j].annotate("series " + str(i), (0.5, 0.5),
+                                xycoords="axes fraction",
+                                ha="center", va="center")
+        # 左端と一番下のサブプロット以外は、軸ラベルを表示しない
+        if i < num_vectors - 1: ax[i][j].xaxis.set_visible(False)
+        if j > 0: ax[i][j].yaxis.set_visible(False)
+        
+# 右下と左上のサブプロットは、テキストのみ表示しているため、
+# 軸ラベルが誤っている。ここで正しく修正する
+ax[-1][-1].set_xlim(ax[0][-1].get_xlim())
+ax[0][0].set_ylim(ax[0][1].get_xlim())
--- code/scratch/linear_algebra.py
+++ code/scratch/linear_algebra.py
@@ -1,6 +1,8 @@
 import math
+from typing import Callable
 
 Vector = list[float]
+Matrix = list[list[float]]
 
 def add(v: Vector, w: Vector) -> Vector:
     """対応する要素の和"""
@@ -68,3 +70,43 @@ def distance(v: Vector, w: Vector) -> float:
     return math.sqrt(squared_distance(v, w))
 def distance(v: Vector, w: Vector) -> float:
     return magnitude(subtract(v, w))
+
+A = [[1, 2, 3],
+     [4, 5, 6]]
+B = [[1, 2],
+     [3, 4],
+     [5, 6]]
+
+def shape(A: Matrix) -> tuple[int, int]:
+    """Aの行数と列数を返す"""
+    num_rows = len(A)
+    num_cols = len(A[0] if A else 0)
+    return num_rows, num_cols
+
+assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3)
+
+def get_row(A: Matrix, i: int) -> Vector:
+    """Aのi番目の行を（ベクトルとして）返す"""
+    return A[i]
+def get_column(A: Matrix, j: int) -> Vector:
+    """Aのj番目の列を（ベクトルとして）返す"""
+    return [A_i[j] for A_i in A]
+
+def make_matrix(num_rows: int,
+                num_cols: int,
+                entry_fn: Callable[[int, int], float]) -> Matrix:
+    """
+    num_rows x num_colsの行列を返す
+    (i, j)の要素は、entry_fn(i, j)が与える
+    """
+    return [[entry_fn(i, j) for j in range(num_cols)] for i in range(num_rows)]
+
+def identity_matrix(n: int) -> Matrix:
+    """n×nの単位行列を返す"""
+    return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
+
+assert identity_matrix(5) == [[1, 0, 0, 0, 0],
+                              [0, 1, 0, 0, 0],
+                              [0, 0, 1, 0, 0],
+                              [0, 0, 0, 1, 0],
+                              [0, 0, 0, 0, 1]]
