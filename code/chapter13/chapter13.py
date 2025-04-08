88aa7e9e43915762bb117d2d6d27819ec847b9f2
Baum-51 <olympos.12z@gmail.com>
2025-04-08 21:24:49 +0900

    20250408 13章まで完了

--- code/chapter13/chapter13.py
+++ code/chapter13/chapter13.py
@@ -106,3 +106,56 @@ p_if_spam = math.exp(sum(math.log(p) for p in probs_if_spam))
 p_if_ham = math.exp(sum(math.log(p) for p in probs_if_ham))
 
 assert model.predict(text) == p_if_spam / (p_if_spam + p_if_ham)
+
+
+import glob, re
+    
+# modify the path to wherever you've put the files
+path = 'spam_data/*/*'
+    
+data: list[Message] = []
+    
+# glob.glob returns every filename that matches the wildcarded path
+for filename in glob.glob(path):
+    is_spam = "ham" not in filename
+    
+    # There are some garbage characters in the emails, the errors='ignore'
+     # skips them instead of raising an exception.
+    with open(filename, errors='ignore') as email_file:
+        for line in email_file:
+            if line.startswith("Subject:"):
+                subject = line.lstrip("Subject: ")
+                data.append(Message(subject, is_spam))
+                break  # done with this file
+    
+import random
+from chapter12.chapter12 import split_data
+    
+random.seed(0)      # just so you get the same answers as me
+train_messages, test_messages = split_data(data, 0.75)
+    
+model = NaiveBayesClassifier()
+model.train(train_messages)
+    
+from collections import Counter
+    
+predictions = [(message, model.predict(message.text))
+                for message in test_messages]
+    
+    # Assume that spam_probability > 0.5 corresponds to spam prediction
+    # and count the combinations of (actual is_spam, predicted is_spam)
+confusion_matrix = Counter((message.is_spam, spam_probability > 0.5)
+                            for message, spam_probability in predictions)
+    
+print(confusion_matrix)
+    
+def p_spam_given_token(token: str, model: NaiveBayesClassifier) -> float:
+    # We probably shouldn't call private methods, but it's for a good cause.
+    prob_if_spam, prob_if_ham = model._probabilities(token)
+    
+    return prob_if_spam / (prob_if_spam + prob_if_ham)
+    
+words = sorted(model.tokens, key=lambda t: p_spam_given_token(t, model))
+    
+print("spammiest_words", words[-10:])
+print("hammiest_words", words[:10])
