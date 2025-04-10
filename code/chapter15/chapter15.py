926096a95756a911d0fdfbaa7d074114b6636310
Baum-51 <olympos.12z@gmail.com>
2025-04-10 21:46:04 +0900

    20250410 15章まで完了

--- code/chapter15/chapter15.py
+++ code/chapter15/chapter15.py
@@ -71,4 +71,145 @@ assert  0.96 < beta[1] <  1.00 # 友人の数
 assert -1.89 < beta[2] < -1.85 # 1日の労働時間
 assert  0.91 < beta[3] <  0.94 # PhDの有無
 
+from chapter14.chapter14 import total_sum_of_squares
+
+def multiple_r_squared(xs: list[Vector], ys: Vector, beta: Vector) -> float:
+    sum_of_squared_errors = sum(error(x, y, beta) ** 2 for x, y in zip(xs, ys))
+    return 1.0 - sum_of_squared_errors / total_sum_of_squares(ys)
+
+assert 0.67 < multiple_r_squared(inputs, daily_minutes_good, beta) < 0.68
+
+from typing import TypeVar, Callable
+
+X = TypeVar('X')       # データの汎用型
+Stat = TypeVar('Stat') # 統計値の汎用型
+
+def bootstrap_sample(data: list[X]) -> list[X]:
+    """len(data)個のデータを無作為に抽出して、置き換える"""
+    return [random.choice(data) for _ in data]
+
+def bootstrap_statistic(data: list[X],
+                        stat_fn: Callable[[list[X]], Stat],
+                        num_sample: int) -> list[Stat]:
+    """dataのブートストラップをnum_sample回stats_fn関数に適用する"""
+    return [stat_fn(bootstrap_sample(data)) for _ in range(num_sample)]
+
+# 100付近のデータ101個
+close_to_100 = [99.5 + random.random() for _ in range(101)]
+# 0付近のデータ50個と、200付近のデータ50個と、
+# 100付近のデータを1個含む合計101個のデータ
+far_from_100 = ([99.5 + random.random()] + 
+                [random.random() for _ in range(50)] +
+                [200 + random.random() for _ in range(50)])
+
+from scratch.statistics import median, standard_deviation
+
+medians_close = bootstrap_statistic(close_to_100, median, 100)
+medians_far   = bootstrap_statistic(far_from_100, median, 100)
+
+assert standard_deviation(medians_close) < 1
+assert standard_deviation(medians_far) > 90
+
+import datetime
+def estimate_sample_beta(pairs: list[tuple[Vector, float]]):
+    x_sample = [x for x, _ in pairs]
+    y_sample = [y for _, y in pairs]
+    beta = least_squares_fit(x_sample, y_sample, learning_rate, 5000, 25)
+    print("bootstrap sample", beta)
+    return beta
+
+random.seed(0)
+
+# 実行には数分かかる
+bootstrap_betas = bootstrap_statistic(list(zip(inputs, daily_minutes_good)), estimate_sample_beta, 100)
+
+bootstrap_standard_errors = [
+    standard_deviation([beta[i] for beta in bootstrap_betas]) for i in range(4)
+]
+
+print(bootstrap_standard_errors)
+
+from scratch.probability import normal_cdf
+
+def p_value(beta_hat_j: float, sigma_hat_j: float) -> float:
+    if beta_hat_j > 0:
+        # 係数が正であれば、大きな値を得る確率を2倍する
+        return 2 * (1 - normal_cdf(beta_hat_j / sigma_hat_j))
+    else:
+        # 係数が負であれば、小さな値を得る確率を2倍する
+        return 2 * normal_cdf(beta_hat_j / sigma_hat_j)
     
+assert p_value( 30.58, 1.27) < 0.001
+assert p_value( 0.972, 0.103) < 0.001
+assert p_value(-1.865, 0.155) < 0.001
+assert p_value( 0.923, 1.249) > 0.4
+
+# alphaはペナルティの効き具合を調整する*ハイパーパラメータ*
+# 「ラムダ」と呼ばれることもあるが、Pythonでは別の意味を持つ
+def ridge_penalty(beta: Vector, alpha: float) -> float:
+    return alpha * dot(beta[1:], beta[1:])
+
+def squared_error_ridge(x: Vector,
+                        y: float,
+                        beta: Vector,
+                        alpha: float) -> float:
+    """betaの誤差とリッジ回帰のペナルティ項を設定する"""
+    return error(x, y, beta) ** 2 + ridge_penalty(beta, alpha)
+
+from scratch.linear_algebra import add
+
+def ridge_penalty_gradient(beta: Vector, alpha: float) -> Vector:
+    """ペナルティ項のみの勾配"""
+    return [0.] + [2 * alpha * beta_j for beta_j in beta[1:]]
+
+def sqerror_ridge_gradient(x: Vector,
+                           y: float,
+                           beta: Vector,
+                           alpha: float) -> Vector:
+    """
+    ペナルティ項を含むi番目の二乗誤差項に対する勾配
+    """
+    return add(sqerror_gradient(x, y, beta), ridge_penalty_gradient(beta, alpha))
+
+def least_squares_fit_ridge(xs: list[Vector],
+                            ys: list[float],
+                            alpha: float,
+                            learning_rate: float,
+                            num_steps: int,
+                            batch_size: int = 1) -> Vector:
+    # Start guess with mean
+    guess = [random.random() for _ in xs[0]]
+
+    for i in range(num_steps):
+        for start in range(0, len(xs), batch_size):
+            batch_xs = xs[start:start+batch_size]
+            batch_ys = ys[start:start+batch_size]
+
+            gradient = vector_mean([sqerror_ridge_gradient(x, y, guess, alpha)
+                                    for x, y in zip(batch_xs, batch_ys)])
+            guess = gradient_step(guess, gradient, -learning_rate)
+
+    return guess
+
+random.seed(0)
+beta_0 = least_squares_fit_ridge(inputs, daily_minutes_good, 0.0, # alpha
+                                learning_rate, 5000, 25)
+
+assert 5 < dot(beta_0[1:], beta_0[1:]) < 6
+assert 0.67 < multiple_r_squared(inputs, daily_minutes_good, beta_0) < 0.69
+
+beta_0_1 = least_squares_fit_ridge(inputs, daily_minutes_good, 0.1, # alpha
+                                learning_rate, 5000, 25)
+assert 4 < dot(beta_0_1[1:], beta_0_1[1:]) < 5
+assert 0.67 < multiple_r_squared(inputs, daily_minutes_good, beta_0_1) < 0.69
+
+beta_1 = least_squares_fit_ridge(inputs, daily_minutes_good, 1, # alpha
+                                learning_rate, 5000, 25)
+assert 3 < dot(beta_1[1:], beta_1[1:]) < 4
+assert 0.67 < multiple_r_squared(inputs, daily_minutes_good, beta_1) < 0.69
+
+beta_10 = least_squares_fit_ridge(inputs, daily_minutes_good, 10, # alpha
+                                learning_rate, 5000, 25)
+assert 1 < dot(beta_10[1:], beta_10[1:]) < 2
+assert 0.5 < multiple_r_squared(inputs, daily_minutes_good, beta_10) < 0.6
+
